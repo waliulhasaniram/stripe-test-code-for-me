d1cc8d2e27b662516c4dfdd205319cae06c0a433
unknown <you@example.com>
2026-05-07 15:35:52 +0600

    new update

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,45 @@
-require('dotenv').config();
+require("dotenv").config();
 const port = process.env.PORT || 3000;
-const cors = require('cors');
-const express = require('express');
-//const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const cors = require("cors");
+const express = require("express");
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
+const uuid = require("uuid").v4;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
 //routes
-app.get('/', async (req, res) => {
-    res.send('Hello stripe');
+app.get("/", async (req, res) => {
+  res.send("Hello stripe");
+});
+
+app.post("/checkout", async (req, res) => {
+    const { product, token } = req.body;
+    console.log("Product", product);
+    console.log("Price", product.price);
+    const idempotencyKey = uuid();
+
+    return stripe.customers.create({
+        email: token.email,
+        source: token.id
+    }).then(customer => {
+        stripe.charges.create({
+            amount: product.price * 100, // Stripe expects amount in cents
+            currency: "usd",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `Purchased the ${product.name}`,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    country: token.card.address_country
+                }
+            }
+        }, {idempotencyKey})
+    }).then(result => res.status(200).json(result))
+    .catch(err => console.log(err));
 });
 
 //listen
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+//import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import StripeCheckout from "react-stripe-checkout";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [product, setProduct] = useState({
+  //   name: "T-shirt",
+  //   price: 20,
+  //   productBy: "facebook",
+  // });
 
   return (
     <>
@@ -16,107 +21,16 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
+          <h1>Stripe payment</h1>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+        <StripeCheckout stripeKey="" token="" name="by react">
+            <button className="btn">Buy now</button>
+        </StripeCheckout>
       </section>
 
       <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
